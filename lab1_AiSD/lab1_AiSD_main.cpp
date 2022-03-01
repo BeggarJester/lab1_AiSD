60bb4a13c34fdd7f446b43c66a09b24b4dd8349d
BeggarJester <spiridonovalex007@mail.ru>
2022-03-01 19:53:52 +0300

    Добавьте файлы проекта.

---
+++ lab1_AiSD/lab1_AiSD_main.cpp
@@ -0,0 +1,64 @@
+﻿#include "lab1_AiSD_MapRBTree.h"
+
+int main()
+{
+	cout << "Create and fill in our test Map based on RBTree step by step:\n\n";
+
+	CustomMap<int, int> MyCustomMap;
+	MyCustomMap.insert(13, 31);
+	MyCustomMap.print();
+	cout << '\n';
+	MyCustomMap.insert(8, 18);
+	MyCustomMap.print();
+	cout << '\n';
+	MyCustomMap.insert(17, 71);
+	MyCustomMap.print();
+	cout << '\n';
+	MyCustomMap.insert(1, 3);
+	MyCustomMap.print();
+	cout << '\n';
+	MyCustomMap.insert(11, 42);
+	MyCustomMap.print();
+	cout << '\n';
+	MyCustomMap.insert(15, 51);
+	MyCustomMap.print();
+	cout << '\n';
+	MyCustomMap.insert(25, 52);
+	MyCustomMap.print();
+	cout << '\n';
+	MyCustomMap.insert(6, 9);
+	MyCustomMap.print();
+	cout << '\n';
+	cout << "Try to delete the node with key 7:\n\n";
+	try {
+		MyCustomMap.remove(7);
+	}
+	catch (const out_of_range error) {
+		cout << error.what();
+	}
+	cout << '\n';
+	cout << "Delete the node with key 17:\n\n";
+	MyCustomMap.remove(17);
+	MyCustomMap.print();
+	cout << '\n';
+	cout << "Return the node with key 17 back:\n\n";
+	MyCustomMap.insert(17, 71);
+	MyCustomMap.print();
+	cout << '\n';
+	cout << "Get the list of Map keys:\n\n";
+	Linked_list<int> map_keys = MyCustomMap.get_keys();
+	while (map_keys.head != NULL) {
+		cout << map_keys.get_head() << " ";
+		map_keys.set_next();
+	}
+	cout << '\n';
+	cout << '\n';
+	cout << "Get the list of Map values:\n\n";
+	Linked_list<int> bye = MyCustomMap.get_values();
+	while (bye.head != NULL) {
+		cout << bye.get_head() << " ";
+		bye.set_next();
+	}
+	return 0;
+}
+
